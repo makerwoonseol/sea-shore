a6a27c71c478871f99d2c59c3b4a52a2c3945fd1
makerwoonseol <101317954+makerwoonseol@users.noreply.github.com>
2026-07-28 21:29:46 +0900

    Update wave.js

--- wave.js
+++ wave.js
@@ -1,6 +1,5 @@
 import { Point } from "./point.js";
 import { Drawing } from "./drawing.js";
-
 export class Wave {
   constructor(index, totalPoints, color) {
     this.index = index;
@@ -10,33 +9,21 @@ export class Wave {
     this.points = [];
     this.trails = [];
     this.delay = 0;
-
     this.isResetWave = false;
     this.resetFinished = false;
   }
-
   resize(stageWidth, stageHeight) {
     this.stageWidth = stageWidth;
     this.stageHeight = stageHeight;
-
     this.centerX = stageWidth / 2;
     this.centerY = stageHeight / 2;
-
     this.pointGap = this.stageWidth / (this.totalPoints - 1);
-
     this.init();
   }
-
   init() {
     this.points = [];
-
     for (let i = 0; i < this.totalPoints; i++) {
-      const point = new Point(
-        this.index + i,
-        this.pointGap * i,
-        0,
-        Math.random() / 3 + 0.4,
-      );
+      const point = new Point(this.index + i, this.pointGap * i, 0, Math.random() / 3 + 0.4);
       this.points[i] = point;
     }
   }
@@ -46,24 +33,20 @@ export class Wave {
       this.points[i].speed = Math.random() * 1.2 + 1;
     }
   }
-
   goBack() {
     for (let i = 0; i < this.totalPoints; i++) {
       this.points[i].targetY = 0;
       this.points[i].speed = Math.random() * 2 + 0.4;
     }
   }
-
   resetWave() {
     this.isResetWave = true;
-
     for (let i = 0; i < this.totalPoints; i++) {
       this.points[i].y = 0;
       this.points[i].targetY = this.stageHeight;
       this.points[i].speed = Math.random() * 1.2 + 1;
     }
   }
-
   draw(ctx) {
     if (this.delay > 0) {
       this.delay--;
@@ -71,25 +54,18 @@ export class Wave {
     }
     for (let i = this.trails.length - 1; i >= 0; i--) {
       const trail = this.trails[i];
-
       this.drawWave(ctx, trail.points, trail.alpha, this.color);
-
       trail.alpha -= 0.001;
-
       if (trail.alpha <= 0) {
         this.trails.splice(i, 1);
       }
     }
-
     let arrivedCount = 0;
-
     for (let i = 0; i < this.totalPoints; i++) {
       let arrived = this.points[i].y === this.points[i].targetY;
-
       if (arrived == true) {
         arrivedCount++;
       }
-
       this.points[i].update();
     }
     // this.drawWave(ctx, this.points, 0.5, "#daecffe8");
@@ -97,27 +73,23 @@ export class Wave {
     // this.drawWave(ctx, this.points, 0.5, "#004cff83");
 
     this.drawWave(ctx, this.points, 0.99, this.color);
-
     if (arrivedCount === this.totalPoints) {
       const trail = [];
-
       for (let i = 0; i < this.totalPoints; i++) {
         trail.push({
           x: this.points[i].x,
-          y: this.points[i].y,
+          y: this.points[i].y
         });
       }
-
       if (this.isResetWave) {
         // "나 다 덮었어!"
         this.resetFinished = true;
         this.isResetWave = false;
         return;
       }
-
       this.trails.push({
         points: trail,
-        alpha: 0.5,
+        alpha: 0.5
       });
       console.log("모두 도착!");
       if (this.isBack == false) {
@@ -129,29 +101,21 @@ export class Wave {
       }
     }
   }
-
   drawWave(ctx, points, alpha, waveColor) {
     ctx.save();
     ctx.globalAlpha = alpha;
     ctx.beginPath();
-
     let prevX = points[0].x;
     let prevY = points[0].y;
-
     ctx.moveTo(prevX, prevY);
-
     ctx.fillStyle = waveColor;
-
     for (let i = 1; i < this.totalPoints; i++) {
       const cx = (prevX + points[i].x) / 2;
       const cy = (prevY + points[i].y) / 2;
-
       ctx.quadraticCurveTo(prevX, prevY, cx, cy);
-
       prevX = points[i].x;
       prevY = points[i].y;
     }
-
     ctx.lineTo(prevX, prevY);
     ctx.lineTo(this.stageWidth, 0);
     ctx.lineTo(0, 0);
